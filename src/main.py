706b2bbad3ac98c5c8f4685192438d664ef28d68
halilbur <halilburak.ozturk@stu.fsm.edu.tr>
2025-05-03 19:20:43 +0300

    Add requirements and initial main script for medsegbench integration
    - Created requirements.txt with necessary dependencies including matplotlib, numpy, scikit-learn, and medsegbench.
    - Implemented main.py to print the version of medsegbench.

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import medsegbench
+from medsegbench import INFO
+
+def main():
+    # Print the version of medsegbench
+    print(f"medsegbench version: {INFO['kvasir']}")
+if __name__ == "__main__":
+    main()
